9b656f1e5c94d356fedc7ab09b3f5bd602c50f00
djm03178 <djm03178@naver.com>
2018-12-19 15:06:15 +0900

    Update DCC_Auction.sol

--- Assignment3/DCC_Auction.sol
+++ Assignment3/DCC_Auction.sol
@@ -16,13 +16,6 @@ contract DCCAuction {
     constructor(uint _tokenPrice) public
     {
         tokenPrice = _tokenPrice;
-        
-        items.push("iphone 7");
-        items.push("Galaxy S9");
-        items.push("iphone 8");
-        items.push("Galaxy Note 9");
-        items.push("iphone X");
-        items.push("LG G7");
     }
     
     function buy() payable public
@@ -44,28 +37,12 @@ contract DCCAuction {
     
     function bid(bytes32 itemName, uint tokens) public
     {
-        uint idx = getItemIndex(itemName);
-        require(idx != uint(-1));
-        
         require(tokens <= buyers[msg.sender].tokens);
         
         buyers[msg.sender].tokens -= tokens;
         myBid[msg.sender][itemName] += tokens;
         if (myBid[msg.sender][itemName] > highestPrice[itemName])
             highestPrice[itemName] = myBid[msg.sender][itemName];
-    }
-    
-    function getItemIndex(bytes32 itemName) view public returns (uint)
-    {
-        for (uint i = 0; i < items.length; i++)
-            if (items[i] == itemName)
-                return i;
-        return uint(-1);
-    }
-    
-    function getItems() view public returns (bytes32[])
-    {
-        return items;
     }
     
     function getTokenPrice() view public returns (uint)
